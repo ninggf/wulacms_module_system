7214294b3241db243889a392d7f0a1322308f461
windywany <windywany@163.com>
2020-03-10 14:14:47 +0800

    requirements check result

--- installer/controllers/IndexController.php
+++ installer/controllers/IndexController.php
@@ -10,17 +10,22 @@
 
 namespace system\installer\controllers;
 
+use wula\cms\CmfModule;
 use wulaphp\io\Response;
 use wulaphp\mvc\controller\Controller;
+use wulaphp\mvc\controller\SessionSupport;
 
 /**
  * Wulacms安装控制器。
  *
+ * @author  Leo Ning <windywany@gmail.com>
  * @package system\installer\controllers
  * @since   3.0
  * @version 3.0
  */
 class IndexController extends Controller {
+    use SessionSupport;
+
     public function beforeRun($action, $refMethod) {
         if (WULACMF_INSTALLED) {
             //安装完成后不能再访问此控制器
@@ -30,7 +35,145 @@ class IndexController extends Controller {
         return parent::beforeRun($action, $refMethod);
     }
 
+    /**
+     * 安装首页
+     *
+     * @return \wulaphp\mvc\view\View
+     */
     public function index() {
-        return 'install index page';
+        $checked[] = ['安全模式', CmfModule::checkEnv('safe_mode', 0)];
+        $checked[] = ['文件上传', CmfModule::checkEnv('file_uploads', 1)];
+        $checked[] = ['输出缓冲区', CmfModule::checkEnv('output_buffering', 0, true)];
+        $checked[] = ['自动开启SESSION', CmfModule::checkEnv('session.auto_start', 0)];
+
+        $checked [] = [
+            'PHP版本',
+            [
+                'required' => '7.1.0+',
+                'checked'  => phpversion(),
+                'pass'     => version_compare('7.1.0', phpversion(), '<=')
+            ]
+        ];
+
+        $pass = extension_loaded('pdo');
+        if ($pass) {
+            $drivers = \PDO::getAvailableDrivers();
+            if (empty ($drivers)) {
+                $pass = false;
+            } else {
+                $pass = in_array('mysql', $drivers);
+            }
+        }
+        $checked [] = [
+            'PDO (mysql)',
+            [
+                'required' => '有',
+                'checked'  => $pass ? '有' : '无',
+                'pass'     => $pass,
+                'optional' => false
+            ]
+        ];
+
+        $pass       = extension_loaded('gd');
+        $checked [] = [
+            'GD',
+            [
+                'required' => '有',
+                'checked'  => $pass ? '有' : '无',
+                'pass'     => $pass,
+                'optional' => false
+            ]
+        ];
+
+        $pass       = extension_loaded('json');
+        $checked [] = [
+            'JSON',
+            [
+                'required' => '有',
+                'checked'  => $pass ? '有' : '无',
+                'pass'     => $pass,
+                'optional' => false
+            ]
+        ];
+
+        $pass       = extension_loaded('mbstring');
+        $checked [] = [
+            'MB String',
+            [
+                'required' => '有',
+                'checked'  => $pass ? '有' : '无',
+                'pass'     => $pass,
+                'optional' => false
+            ]
+        ];
+
+        $pass       = extension_loaded('curl') && version_compare('7.30.0', curl_version()['version'], '<=');
+        $checked [] = [
+            'Curl',
+            [
+                'required' => '7.30.0+',
+                'checked'  => $pass ? curl_version()['version'] : '无',
+                'pass'     => $pass,
+                'optional' => false
+            ]
+        ];
+
+        $pass       = extension_loaded('redis');
+        $checked [] = [
+            'Redis',
+            [
+                'required' => '可选',
+                'checked'  => $pass ? '有' : '无',
+                'pass'     => $pass,
+                'optional' => true
+            ]
+        ];
+
+        $f     = TMP_PATH;
+        $rst   = ['tmp目录', CmfModule::checkFile($f)];
+        $dir[] = $rst;
+
+        if ($rst[1]['pass'] && !is_file(TMP_PATH . 'install.txt')) {
+            $code = md5(rand_str(8));//写入安全码
+            @file_put_contents(TMP_PATH . 'install.txt', $code);
+        }
+
+        $f     = LOGS_PATH;
+        $dir[] = ['logs目录', CmfModule::checkFile($f)];
+        $f     = CONFIG_PATH;
+        $dir[] = ['conf目录', CmfModule::checkFile($f)];
+
+        $step = sess_get('step', 'home');
+
+        return view(['requirements' => $checked, 'dirs' => $dir, 'step' => $step]);
+    }
+
+    /**
+     * 验证安全码
+     * @return array
+     */
+    public function verify() {
+        return ['verified' => 1];
+    }
+
+    /**
+     * 环境与语言选项
+     *
+     * @param string $step
+     *
+     * @return array
+     */
+    public function setup($step) {
+        $_SESSION['step'] = $step;
+
+        return ['status' => 1, 'step' => $step];
+    }
+
+    /**
+     * 安装
+     * @return array
+     */
+    public function install() {
+        return ['ok' => 1];
     }
 }
---
+++ installer/views/index/index.tpl
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>安装</title>
+</head>
+<body>
+<h3>依赖检测</h3>
+<pre>{$requirements|json_encode:384}</pre>
+<h3>目录权限</h3>
+<pre>{$dirs|json_encode:384}</pre>
+</body>
+</html>
